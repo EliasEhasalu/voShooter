6a41b8440c5c1c5f574ee1cc19f95fb234592432
Gregor Grigorjan <grgrig@ttu.ee>
2021-02-18 16:54:28 +0200

    gtm

--- core/src/ee/taltech/voshooter/Game.java
+++ core/src/ee/taltech/voshooter/Game.java
@@ -4,31 +4,55 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public final class Game extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Texture img;
+    private Texture texture;
+    private Sprite sprite;
 
+    /**
+     * Used to initialize assets.
+     */
     @Override
     public void create() {
+        // Initialize sprite layer.
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        // Initialize textures.
+        texture = new Texture("badlogic.jpg");
+
+        // Initialize sprites.
+        final int initialPos = 200;
+
+        sprite = new Sprite(texture);
+        sprite.setPosition(initialPos, initialPos);
     }
 
+    /**
+     * Used as a rendering loop.
+     */
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
-        batch.draw(img, 0, 0);
+
+        // Draw sprite onto batch.
+        sprite.draw(batch);
+
         batch.end();
     }
 
+    /**
+     * Used to free memory of assets.
+     */
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        texture.dispose();
     }
 }
