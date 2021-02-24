500ae2b4ad55c0cc5510dc56d05df88abf7191d6
Gregor Grigorjan <grgrig@ttu.ee>
2021-02-24 14:19:22 +0200

    fix main menu flicker

--- core/src/main/java/ee/taltech/voshooter/screens/MenuScreen.java
+++ core/src/main/java/ee/taltech/voshooter/screens/MenuScreen.java
@@ -2,6 +2,7 @@ package ee.taltech.voshooter.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -59,14 +60,21 @@ public class MenuScreen implements Screen {
      */
     @Override
     public void render(float delta) {
+        // Refresh the graphics renderer every cycle.
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // And draw over it again.
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));  // Cap menu FPS to 30.
         stage.draw();
     }
 
+    /**
+     * Make sure the window doesn't break.
+     */
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
