93af1729a6aaee3bfb916f6c8e149d40cc973a93
Juss <jupold@ttu.ee>
2021-02-24 19:55:29 +0200

    #17 added preference screen.

--- core/src/main/java/ee/taltech/voshooter/screens/MenuScreen.java
+++ core/src/main/java/ee/taltech/voshooter/screens/MenuScreen.java
@@ -27,8 +27,6 @@ public class MenuScreen implements Screen {
 
         // Create stage which will contain this screen's objects
         stage = new Stage(new ScreenViewport());
-        // Have it handle player's input.
-        Gdx.input.setInputProcessor(stage);
     }
 
     /**
@@ -36,6 +34,8 @@ public class MenuScreen implements Screen {
      */
     @Override
     public void show() {
+        // Have it handle player's input.
+        Gdx.input.setInputProcessor(stage);
         // Add a table which will contain menu items to the stage.
         Table table = new Table();
         table.setFillParent(true);
@@ -59,6 +59,12 @@ public class MenuScreen implements Screen {
         table.row();
         table.add(exit).fillX().uniformX();
 
+        preferences.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.changeScreen(VoShooter.Screen.PREFERENCES);
+            }
+        });
         exit.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
