c3011962d0136aef9527a826e45ea5acb96d3a9a
elehas <elehas@ttu.ee>
2021-02-28 18:11:42 +0200

    Missing import statement.

--- core/src/main/java/ee/taltech/voshooter/screens/CreateGameScreen.java
+++ core/src/main/java/ee/taltech/voshooter/screens/CreateGameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
