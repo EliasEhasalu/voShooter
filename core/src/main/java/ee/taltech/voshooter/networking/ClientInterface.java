e245c79889a3a7266dda3a601342fedf3cb05ac5
Gregor Grigorjan <grgrig@ttu.ee>
2021-03-08 16:09:49 +0200

    delete unnecessary file

--- core/src/main/java/ee/taltech/voshooter/networking/ClientInterface.java
+++ core/src/main/java/ee/taltech/voshooter/networking/ClientInterface.java
@@ -1,5 +1,7 @@
 package ee.taltech.voshooter.networking;
 
+import ee.taltech.voshooter.networking.messages.User;
+
 /**
  * This interface will need to be implemented by VoClient.
  * It will ensure that server requests are handled properly on the client.
--- core/src/main/java/ee/taltech/voshooter/networking/User.java
+++
@@ -1,5 +0,0 @@
-package ee.taltech.voshooter.networking;
-
-public class User {
-    public String name;
-}
