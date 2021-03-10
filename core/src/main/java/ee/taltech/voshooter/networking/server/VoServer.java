ac0b204de381d4aac574dc6b16729a11c3e18e18
Gregor Grigorjan <grgrig@ttu.ee>
2021-03-10 15:30:46 +0200

    refactor server code

--- core/src/main/java/ee/taltech/voshooter/networking/server/VoServer.java
+++ core/src/main/java/ee/taltech/voshooter/networking/server/VoServer.java
@@ -47,104 +47,154 @@ public class VoServer {
 
         Network.register(server);
 
-        server.addListener(new Listener() {
-
+        server.addListener(
+        new RunMethodListener<SetUsername>(SetUsername.class) {
             @Override
-            public void received(Connection c, Object message) {
-                VoConnection connection = (VoConnection) c;
-                if (connection.user == null) {
-                    connection.user = new User();
-                    connection.user.id = playerId++;
-                }
-                connections.add(connection);
-                User user = connection.user;
-
-
-                if (message instanceof SetUsername) {
-                    user.name = ((SetUsername) message).desiredName;
-                    return;
-                }
-
-                if (message instanceof CreateLobby) {
-                    CreateLobby req = (CreateLobby) message;
-                    String code = generateLobbyCode();
-                    Lobby newLobby = new Lobby(req.gameMode, req.maxPlayers, code);
-                    newLobby.setHost(connection);
-                    lobbies.put(code, newLobby);
-
-                    if (newLobby.addConnection(connection)) {
-                       user.currentLobby = code;
-                    }
-
-                    LobbyJoined res = new LobbyJoined(req.gameMode, req.maxPlayers, code, newLobby.getUsers(), user);
-                    c.sendTCP(res);
-                    return;
-                }
-
-                if (message instanceof JoinLobby) {
-                    String code = ((JoinLobby) message).lobbyCode.trim().toUpperCase();
-                    if (code != null && code != "" && lobbies.containsKey(code)) {
-                        Lobby lobby = lobbies.get(code);
-                        if (!lobby.addConnection(connection)) {
-                            connection.sendTCP(new LobbyFull());
-                        } else {
-                            int gameMode = lobby.getGameMode();
-                            int maxPlayers = lobby.getMaxPlayers();
-                            List<User> users = lobby.getUsers();
-                            User host = lobby.getHost().user;
-                            user.currentLobby = code;
-
-                            connection.sendTCP(new LobbyJoined(gameMode, maxPlayers, code, users, host));
-                            lobby.addConnection(connection);
-                        }
-                    } else {
-                        connection.sendTCP(new NoSuchLobby());
-                    }
-                    return;
-                }
-
-                if (message instanceof LeaveLobby) {
-                    Lobby lobby = lobbies.get(user.currentLobby);
-                    lobby.removeConnection(connection);
-
-                    if (lobby.getPlayerCount() == 0) {
-                        lobbies.remove(lobby.getLobbyCode());
-                    }
-                }
-            }
-
-            @Override
-            public void disconnected(Connection c) {
-               VoConnection connection = (VoConnection) c;
-               if (connection != null) {
-                   if (connections.contains(connection)) connections.remove(connection);
-                   if (connection.user != null && connection.user.currentLobby != null) {
-                       Lobby lobby = lobbies.get(connection.user.currentLobby);
-                       lobby.removeConnection(connection);
-                   }
-               }
+            public void run(VoConnection c, SetUsername msg) {
+                handleSetUsername(c, msg);
             }
         });
 
-        // server.addListener(
-        // new RunMethodListener<LeaveLobby>(LeaveLobby.class) {
-        //     @Override
-        //     public void run(Connection c, LeaveLobby msg) {
-        //     }
-        // });
+        server.addListener(new Listener() {
+            @Override
+            public void disconnected(Connection c) {
+                handleDisconnects((VoConnection) c);
+            }
+        });
+
+        server.addListener(
+        new RunMethodListener<CreateLobby>(CreateLobby.class) {
+            @Override
+            public void run(VoConnection c, CreateLobby msg) {
+                handleCreateLobby(c, msg);
+            }
+        });
+
+        server.addListener(
+        new RunMethodListener<JoinLobby>(JoinLobby.class) {
+            @Override
+            public void run(VoConnection c, JoinLobby msg) {
+                handleJoinLobby(c, msg);
+            }
+        });
+
+        server.addListener(
+        new RunMethodListener<LeaveLobby>(LeaveLobby.class) {
+            @Override
+            public void run(VoConnection c, LeaveLobby msg) {
+                handleLeaveLobby(c);
+            }
+        });
 
         server.bind(Network.PORT);
         server.start();
     }
 
+    /**
+     * Handle creating lobbies.
+     * @param connection The connection.
+     * @param msg The message.
+     */
+    private void handleCreateLobby(VoConnection connection, CreateLobby msg) {
+        String code = generateLobbyCode();
+        User user = connection.user;
+        Lobby newLobby = new Lobby(msg.gameMode, msg.maxPlayers, code);
+        newLobby.setHost(connection);
+        lobbies.put(code, newLobby);
 
-    /** Close the server upon request. */
-    public void close() {
-        try {
-            server.dispose();
-        } catch (IOException e) {
-            //.
+        if (newLobby.addConnection(connection)) {
+           user.currentLobby = code;
         }
+
+        LobbyJoined res = new LobbyJoined(msg.gameMode, msg.maxPlayers, code, newLobby.getUsers(), user);
+        connection.sendTCP(res);
+    }
+
+    /**
+     * @param connection The connection.
+     * @param msg The JoinLobby message {@link JoinLobby}
+     */
+    private void handleJoinLobby(VoConnection connection, JoinLobby msg) {
+        String code = ((JoinLobby) msg).lobbyCode.trim().toUpperCase();
+        User user = connection.user;
+
+        if (code != null && code != "" && lobbies.containsKey(code)) {
+            Lobby lobby = lobbies.get(code);
+            if (!lobby.addConnection(connection)) {
+                connection.sendTCP(new LobbyFull());
+            } else {
+                int gameMode = lobby.getGameMode();
+                int maxPlayers = lobby.getMaxPlayers();
+                List<User> users = lobby.getUsers();
+                User host = lobby.getHost().user;
+                user.currentLobby = code;
+
+                connection.sendTCP(new LobbyJoined(gameMode, maxPlayers, code, users, host));
+                lobby.addConnection(connection);
+            }
+        } else {
+            connection.sendTCP(new NoSuchLobby());
+        }
+    }
+
+    /**
+     * @param connection The connection.
+     */
+    private void handleLeaveLobby(VoConnection connection) {
+        Lobby lobby = lobbies.get(connection.user.currentLobby);
+        lobby.removeConnection(connection);
+
+        if (lobby.getPlayerCount() == 0) {
+            lobbies.remove(lobby.getLobbyCode());
+        }
+    }
+
+    /**
+     * @param connection The connection.
+     * @param msg The message with the desired username.
+     */
+    private void handleSetUsername(VoConnection connection, SetUsername msg) {
+        handleNewConnections(connection);
+        if (verifyUsername(msg.desiredName)) connection.user.name = msg.desiredName;
+    }
+
+    /**
+     * Create and save a User object if one doesn't already exist for this connection.
+     * @param connection The connection being examined.
+     */
+    private void handleNewConnections(VoConnection connection) {
+        if (connection.user == null) {
+            connection.user = new User();
+            connection.user.id = playerId++;
+        }
+        if (!connections.contains(connection)) connections.add(connection);
+    }
+
+    /**
+     * Handle severed connections.
+     * @param connection The connection that disconnected.
+     */
+    private void handleDisconnects(VoConnection connection) {
+       if (connection != null) {
+           if (connections.contains(connection)) connections.remove(connection);
+           if (connection.user != null && connection.user.currentLobby != null) {
+               Lobby lobby = lobbies.get(connection.user.currentLobby);
+               lobby.removeConnection(connection);
+           }
+       }
+    }
+
+    /**
+     * @param username The username to check.
+     * @return Whether the username is acceptable.
+     */
+    private boolean verifyUsername(String username) {
+        return (
+            !username.trim().equals("")
+            && username != null
+            && username.length() >= 4
+            && username.length() <= 12
+        );
     }
 
     /** @return A unique lobby code for a newly created lobby. */
@@ -168,6 +218,15 @@ public class VoServer {
             break;
         }
         return attempt;
+    }
+
+    /** Close the server upon request. */
+    public void close() {
+        try {
+            server.dispose();
+        } catch (IOException e) {
+            //.
+        }
     }
 
     static class VoConnection extends Connection {
