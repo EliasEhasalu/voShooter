9597ce3430c0262af0dec143bf8537a049b4c075
Gregor Grigorjan <grgrig@ttu.ee>
2021-03-10 13:53:14 +0000

    Update core/src/main/java/ee/taltech/voshooter/screens/LobbyScreen.java

--- core/src/main/java/ee/taltech/voshooter/screens/LobbyScreen.java
+++ core/src/main/java/ee/taltech/voshooter/screens/LobbyScreen.java
@@ -3,7 +3,6 @@ package ee.taltech.voshooter.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.physics.bullet.collision._btMprSimplex_t;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
