548b4ab1c16c4cf4153ff19a5ba45668afb3cb62
Gregor Grigorjan <grgrig@ttu.ee>
2021-03-20 16:40:08 +0200

    make connection fields transient just in case

--- core/src/main/java/ee/taltech/voshooter/networking/server/VoConnection.java
+++ core/src/main/java/ee/taltech/voshooter/networking/server/VoConnection.java
@@ -7,6 +7,6 @@ import ee.taltech.voshooter.networking.messages.User;
 
 public class VoConnection extends Connection {
 
-    public User user;
-    public Player player;
+    public transient User user;
+    public transient Player player;
 }
