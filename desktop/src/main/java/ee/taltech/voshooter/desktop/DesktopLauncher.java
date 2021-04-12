92df8d9c54c9fbc817d8550a088faf6d569fbea5
elehas <elehas@ttu.ee>
2021-04-12 22:44:50 +0300

    Change jar file name and icon.

--- desktop/src/main/java/ee/taltech/voshooter/desktop/DesktopLauncher.java
+++ desktop/src/main/java/ee/taltech/voshooter/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package ee.taltech.voshooter.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ee.taltech.voshooter.VoShooter;
@@ -13,6 +14,7 @@ public class DesktopLauncher {
      */
     public static void main(String[] args) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.addIcon("textures/playerIcon.png", Files.FileType.Classpath);
         config.width = 1600;
         config.height = 900;
         new LwjglApplication(new VoShooter(args), config);
