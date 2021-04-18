7ec20a500f95ae40ef609fa375f2ac9e9f2b3e75
Gregor Grigorjan <grgrig@ttu.ee>
2021-04-18 18:11:12 +0300

    tweak flamethrower

--- core/src/main/java/ee/taltech/voshooter/weapon/projectile/Fireball.java
+++ core/src/main/java/ee/taltech/voshooter/weapon/projectile/Fireball.java
@@ -7,9 +7,9 @@ import ee.taltech.voshooter.networking.server.gamestate.player.Player;
 public class Fireball extends Bullet {
 
     public static final float RADIUS = 0.5f;
-    private static final float SPEED = 10f;
-    private static final float LIFE_TIME = 0.7f;
-    private static final int DAMAGE = 5;
+    private static final float SPEED = 30f;
+    private static final float LIFE_TIME = 0.23f;
+    private static final int DAMAGE = 3;
 
     /**
      * Construct the projectile.
