dc5b6e6864a84ae519e015ea07a6a4553fddba77
Gregor Grigorjan <grgrig@ttu.ee>
2021-04-22 23:18:08 +0300

    added railgun yay

--- core/src/main/java/ee/taltech/voshooter/weapon/hitscanweapon/HitscanWeapon.java
+++ core/src/main/java/ee/taltech/voshooter/weapon/hitscanweapon/HitscanWeapon.java
@@ -1,12 +1,37 @@
 package ee.taltech.voshooter.weapon.hitscanweapon;
 
+import com.badlogic.gdx.physics.box2d.Body;
+import ee.taltech.voshooter.networking.server.gamestate.collision.utils.RayCaster;
 import ee.taltech.voshooter.networking.server.gamestate.player.Player;
 import ee.taltech.voshooter.networking.server.gamestate.player.status.DamageDealer;
 import ee.taltech.voshooter.weapon.Weapon;
 
+import java.util.HashSet;
+
 public abstract class HitscanWeapon extends Weapon implements DamageDealer {
 
-    public HitscanWeapon(Player wielder, float coolDown, int startingAmmo, Weapon.Type type) {
+    private final RayCaster rayCaster = new RayCaster();
+    private final float maxDist;
+    private final int damage;
+
+    public HitscanWeapon(Player wielder, float coolDown, int startingAmmo, float maxDist, int damage, Weapon.Type type) {
         super(wielder, coolDown, startingAmmo, type);
+        this.maxDist = maxDist;
+        this.damage = damage;
+    }
+
+    protected void onFire() {
+        Body hitObject = rayCaster.getFirstCollision(
+                wielder.getWorld(),
+                wielder.getPos(),
+                wielder.getViewDirection(),
+                maxDist,
+                new HashSet<Body>() {{ add(wielder.getBody()); }}
+        );
+
+        if (hitObject != null && hitObject.getUserData() instanceof Player) {
+            Player hitPlayer = (Player) hitObject.getUserData();
+            hitPlayer.takeDamage(damage, this);
+        }
     }
 }
