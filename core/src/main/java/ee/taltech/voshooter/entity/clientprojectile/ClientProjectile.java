95e055f2158755c9ef1869f301bb1861670c4a43
Gregor Grigorjan <grgrig@ttu.ee>
2021-04-28 11:57:49 +0300

    add flamethrower sound

--- core/src/main/java/ee/taltech/voshooter/entity/clientprojectile/ClientProjectile.java
+++ core/src/main/java/ee/taltech/voshooter/entity/clientprojectile/ClientProjectile.java
@@ -34,6 +34,7 @@ public class ClientProjectile extends Entity implements Drawable {
         put(Projectile.Type.PISTOL_BULLET, "soundfx/gun/weapon1.ogg");
         put(Projectile.Type.GRENADE, "soundfx/gun/grenade_launcher.ogg");
         put(Projectile.Type.SHOTGUN_PELLET, "soundfx/gun/shotgun.ogg");
+        put(Projectile.Type.FIREBALL, "soundfx/gun/flame.ogg");
     }};
 
     private static final Map<Projectile.Type, String> SOUND_DESTROYED = new HashMap<Projectile.Type, String>() {{
