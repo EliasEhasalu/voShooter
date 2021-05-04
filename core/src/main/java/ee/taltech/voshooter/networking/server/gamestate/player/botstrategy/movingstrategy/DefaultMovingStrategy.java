c6e3e7498d62ca431449ee486dbc373a8850f93a
Gregor Grigorjan <grgrig@ttu.ee>
2021-05-04 23:59:31 +0300

    add bfs pathfinding to bots

--- core/src/main/java/ee/taltech/voshooter/networking/server/gamestate/player/botstrategy/movingstrategy/DefaultMovingStrategy.java
+++ core/src/main/java/ee/taltech/voshooter/networking/server/gamestate/player/botstrategy/movingstrategy/DefaultMovingStrategy.java
@@ -1,8 +1,14 @@
 package ee.taltech.voshooter.networking.server.gamestate.player.botstrategy.movingstrategy;
 
 import ee.taltech.voshooter.networking.server.gamestate.collision.utils.LevelGenerator;
+import ee.taltech.voshooter.networking.server.gamestate.collision.utils.Node;
+import ee.taltech.voshooter.networking.server.gamestate.collision.utils.PathFinding;
+import ee.taltech.voshooter.networking.server.gamestate.collision.utils.PixelToSimulation;
 import ee.taltech.voshooter.networking.server.gamestate.player.Bot;
+import ee.taltech.voshooter.networking.server.gamestate.player.Player;
+import ee.taltech.voshooter.networking.server.gamestate.player.botstrategy.BotStrategy;
 
+import java.util.List;
 import java.util.Random;
 
 public class DefaultMovingStrategy implements MovingStrategy {
@@ -10,9 +16,11 @@ public class DefaultMovingStrategy implements MovingStrategy {
     private static final Random R = new Random();
     private Bot bot;
     private transient int[][] walls;
+    private transient BotStrategy parent;
 
     @Override
-    public int[] getMovementDirections() {
+    public int[] getMovementDirections(Player closestEnemy) {
+        if (closestEnemy != null) return navigateToTarget(closestEnemy);
         return new int[] {R.nextInt(3) - 1, R.nextInt(3) - 1};
     }
 
@@ -20,5 +28,18 @@ public class DefaultMovingStrategy implements MovingStrategy {
     public void setBot(Bot bot) {
         this.bot = bot;
         this.walls = LevelGenerator.getWallGrid(bot.getGame().getCurrentMap());
+    }
+
+    private int[] navigateToTarget(Player enemy) {
+        List<Node> nodes = getPathTo(enemy);
+        if (nodes.size() <= 1) return new int[] {0, 0};
+        else return nodes.get(1).subtract(nodes.get(0));
+    }
+
+    private List<Node> getPathTo(Player enemy) {
+        Node start = new Node(PixelToSimulation.castToGrid(bot.getPos()), 0);
+        Node target = new Node(PixelToSimulation.castToGrid(enemy.getPos()), 0);
+
+        return PathFinding.bfsPath(start, target, walls);
     }
 }
